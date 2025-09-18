1380ca991da817b32438080a87b8208e763e96cf
Dragaspetar00 <dragaspetar@protonmail.com>
2025-09-18 19:43:47 +0300

    v

--- sw.js
+++ sw.js
@@ -1,26 +1,20 @@
-const CACHE_NAME = 'not-defteri-v2';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/app.js',
-    '/manifest.json',
-    '/icons/icon-192.png',
-    '/icons/icon-512.png'
+const CACHE = 'not-defteri-v3';
+const TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://dummyimage.com/192x192/ffffff/000000.png&text=ND',
+  'https://dummyimage.com/300x300/ffffff/000000.png&text=Not+Defteri'
 ];
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(TO_CACHE))
+  );
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
-    );
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
 });
